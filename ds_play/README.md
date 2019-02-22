59df91a1b8f871dee523b0a9dfb1ebc8a9f0a9e9
Xiangkui Guo <bruce1988cn@gmail.com>
2019-02-22 11:23:15 +0800

    init ds env

---
+++ ds_play/README.md
@@ -0,0 +1,1 @@
+# data science with anaconda and py3
---
+++ ds_play/initds.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array((1,2,3,4,5,6))
+y = np.array((2,3,4,4,-1,23))
+
+plt.plot(x,y)
+plt.show()
