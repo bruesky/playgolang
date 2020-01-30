3b26f295c4187d9bedd7923b4c359bfae23c7e6e
Xiangkui Guo <bruce1988cn@gmail.com>
2020-01-30 12:17:34 -0600

    C#

---
+++ LearnCSharp/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte num = 255;
+            Console.WriteLine(num+1);
+        }
+    }
+}
